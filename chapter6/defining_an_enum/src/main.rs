cc73f4875c7886aa2da9402e9b044f8e21cf85c6
Taffarel Oliveira <115137992+kirakernel@users.noreply.github.com>
2023-10-29 21:49:01 +0000

    defining an `enum`

---
+++ chapter6/defining_an_enum/src/main.rs
@@ -0,0 +1,14 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    //route(four);
+    //route(six)
+}
+
+fn route(ip_kind: IpAddrKind) {}
---
+++ chapter6/option_enum/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let some_number = Some(5);
+    let some_char = Some('e');
+    let some_str = Some("string");
+
+    let absent_number: Option<i32> = None;
+}
