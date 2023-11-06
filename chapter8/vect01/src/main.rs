6b9752d759022f9eeb8f7b86f524f02074d2de1a
Taffarel Oliveira <115137992+kirakernel@users.noreply.github.com>
2023-11-06 22:01:29 +0000

    Storing lists of values with vectors

---
+++ chapter8/vect01/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // let v: Vec<i32> = Vec::new();
+    // let v = vec![1,2,3,4];
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    println!("{:?}", v);
+}
