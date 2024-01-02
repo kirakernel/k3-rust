d859d77fdcd9c27b0d66a4124c524b36ee6660a4
kirakernel <115137992+kirakernel@users.noreply.github.com>
2024-01-01 22:21:01 -0300

    Defining an Enum

---
+++ chapter06/message/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("You called me!");
+    }
+}
+
+fn main() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    println!("{:?}", m);
+}
