950829ba23200b487c5af82a75501489893a7c62
kirakernel <115137992+kirakernel@users.noreply.github.com>
2024-01-12 02:27:00 -0300

    Bringing paths into scope with the use keyword

--- chapter07/restaurant/src/lib.rs
+++ chapter07/restaurant/src/lib.rs
@@ -31,9 +31,11 @@ mod back_of_house {
     fn cook_order() {}
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
