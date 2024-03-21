660c06e5423b72f17d837f8989c762674f482a24
Taffarel Oliveira <taffareloliveira60@gmail.com>
2024-03-20 21:23:29 -0300

    Advanced traits

---
+++ chapter19/associated_types/src/main.rs
@@ -0,0 +1,73 @@
+/*pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}*/
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+struct User<'a> {
+    id: &'a str,
+    name: &'a str,
+    n_fields: u32,
+}
+
+impl<'a> User<'a> {
+    fn new(id: &'a str, name: &'a str) -> Self {
+        Self {
+            id,
+            name,
+            n_fields: 2,
+        }
+    }
+}
+
+impl<'a> Iterator for User<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.n_fields == 2 {
+            self.n_fields -= 1;
+            Some(self.name)
+        } else if self.n_fields == 1 {
+            self.n_fields -= 1;
+            Some(self.id)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let user = User::new("01122222225988988", "Amy");
+
+    for elem in user {
+        println!("{:?}", elem);
+    }
+
+    let counter = Counter::new();
+    for i in counter {
+        println!("i={}", i);
+    }
+}
